5d4aa4a1ac0deca14a51bdfe4881a1e360362f25
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-05-20 22:38:12 -0300

    Validação de tamanho mínimo do campo 'title'

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 3 }
 
   enum :status, { to_do: 0, doing: 1, done: 2 }
 end
