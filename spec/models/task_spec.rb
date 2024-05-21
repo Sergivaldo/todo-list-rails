451395df1d552f0b334d8e659907c82ac557d4af
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-05-20 22:40:01 -0300

    Teste de validação de tamanho mínimo

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Task, type: :model do
 
   describe 'Validation' do
     it { should validate_presence_of(:title) }
-    
+    it {
+      should validate_length_of(:title)
+        .is_at_least(3)
+    }
   end
 end
