db21adae63120c106c7714d484898e0c009eccbc
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-05-20 22:21:11 -0300

    Migration de criação de tabela 'tasks'

--- db/migrate/20240521011047_create_tasks.rb
+++ db/migrate/20240521011047_create_tasks.rb
@@ -1,10 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.string :title, null: false
       t.date :due_date
-      t.integer :status
-
+      t.integer :status, default: 0
       t.timestamps
     end
   end
