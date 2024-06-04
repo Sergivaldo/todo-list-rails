904705ae8efc929768812bc565950a62ef2c0b39
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-06-04 17:59:15 -0300

    remoção de validação de presença

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3 }
+  validates :title, length: { minimum: 3 }
 
   enum :status, { to_do: 0, doing: 1, done: 2 }
 end
