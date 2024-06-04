e1a7be2340113067113a7b81783113df9b1794c3
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-06-04 17:44:24 -0300

    remoção de método de validação

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Task, type: :model do
   subject(:task) { build :task }
 
   describe 'Validation' do
-    it { should validate_presence_of(:title) }
     it {
       should validate_length_of(:title)
         .is_at_least(3)
